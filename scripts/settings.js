f0460a950105fcf030dfcd953a0d618031e49c4c
oberlin-t <thomas.l.oberlin@gmail.com>
2021-01-08 12:14:22 -0500

    refactored js and improved settings

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,45 +1,44 @@
-function fgColor(fg) {
-    
-    let divElements = document.getElementsByTagName('div');
-    let navElements = document.getElementsByTagName('a'); //this might hit some other hyperlinks again with styling though its not clear if that matters?
-   
-    for (var i=0; i<divElements.length; i++){
-        divElements[i].style.color = "#" + String(fg);
-    }
-    
-    for (var j=0; j<navElements.length; j++){
-        navElements[j].style.color = "#" + String(fg);
-    }
-}
-
-function bgColor(bg){
-    document.body.style.background = "#" + String(bg);
+function setColor(cssvar,color) {
+    document.documentElement.style.setProperty(cssvar, '#' + String(color));
 }
 
 function grabColors() {
     //declare neccacary variables
     let fgcol = document.getElementById('fgcolor').value;
     let bgcol = document.getElementById('bgcolor').value;
-
+    let secol = document.getElementById('secolor').value;
+    let tertcol = document.getElementById('tertcolor').value;
+    
     //set colors
-    fgColor(fgcol);
-    bgColor(bgcol);
+    setColor('--bg-color',bgcol);
+    setColor('--fg-color',fgcol);
+    setColor('--secondary-fg',secol);
+    setColor('--tertiary-fg',tertcol);
     //save settings in localStorage
-    localStorage.setItem("background", String(bgcol))
-    localStorage.setItem("foreground", String(fgcol))
+    if (String(bgcol) != ""){
+        localStorage.setItem("background", String(bgcol))
+    }
+    if (String(fgcol) != ""){
+        localStorage.setItem("foreground", String(fgcol))
+    }
+    if (String(secol) != ""){
+        localStorage.setItem("secondary", String(secol))
+    }
+    if (String(tertcol) != "") {
+        localStorage.setItem("tertiary", String(tertcol))
+    }
 }
 
-function loadCookie(){
-    
+function loadColors(){
     var fg = localStorage.getItem("foreground");
     var bg = localStorage.getItem("background");
-
-    if (localStorage.getItem("foreground") != ""){
-        fgColor(fg);
-    }
-    if (localStorage.getItem("background") != ""){
-        bgColor(bg);
-    }
+    var se = localStorage.getItem("secondary");
+    var te = localStorage.getItem("tertiary");
+    
+    setColor('--fg-color',fg);
+    setColor('--bg-color',bg);
+    setColor('--secondary-fg',se);
+    setColor('--tertiary-fg',te);
 }
 
 
