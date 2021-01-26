4199795b1126c2a7b54b6bb011ac3a634a582211
oberlin-t <thomas.l.oberlin@gmail.com>
2021-01-25 19:11:27 -0500

    removed secondary and tertiary colors

--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -6,8 +6,6 @@ function grabColors() {
     //declare neccacary variables
     let fgcol = document.getElementById('fgcolor').value;
     let bgcol = document.getElementById('bgcolor').value;
-    let secol = document.getElementById('secolor').value;
-    let tertcol = document.getElementById('tertcolor').value;
     
     //save settings in localStorage and set colors
     if (String(bgcol) != ""){
@@ -17,14 +15,6 @@ function grabColors() {
     if (String(fgcol) != ""){
         localStorage.setItem("foreground", String(fgcol))
         setColor('--fg-color',fgcol);
-    }
-    if (String(secol) != ""){
-        localStorage.setItem("secondary", String(secol))
-        setColor('--secondary-fg',secol);
-    }
-    if (String(tertcol) != "") {
-        localStorage.setItem("tertiary", String(tertcol))
-        setColor('--tertiary-fg',tertcol);
     }
 }
 
@@ -36,8 +26,6 @@ function loadColors(){
     
     setColor('--fg-color',fg);
     setColor('--bg-color',bg);
-    setColor('--secondary-fg',se);
-    setColor('--tertiary-fg',te);
-}
+ }
 
 
