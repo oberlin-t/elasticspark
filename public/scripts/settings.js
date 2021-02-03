7152da89ab159bb3bd19f2afc990c8ba110451c1
oberlin-t <thomas.l.oberlin@gmail.com>
2021-02-02 22:31:20 -0500

    allowed colors to save

--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -1,6 +1,19 @@
 function setColor(cssvar,color) {
-    document.documentElement.style.setProperty(cssvar, String(color));
+    document.documentElement.style.setProperty(cssvar, color);
 }
+
+//I hate this function. It is far to much work for what it does.
+function setColorChooser(id){
+    if (localStorage.getItem("foreground") && localStorage.getItem("background") != null){
+        if(id == "fgcolor"){
+            document.getElementById(id).value = localStorage.getItem("foreground");
+        }
+        if(id == "bgcolor"){
+            document.getElementById(id).value = localStorage.getItem("background");
+        }
+    }
+}
+
 
 function grabColors() {
     //declare neccacary variables
@@ -9,11 +22,11 @@ function grabColors() {
     
     //save settings in localStorage and set colors
     if (String(bgcol) != ""){
-        localStorage.setItem("background", String(bgcol))
+        localStorage.setItem("background", bgcol)
         setColor('--bg-color',bgcol);
     }
     if (String(fgcol) != ""){
-        localStorage.setItem("foreground", String(fgcol))
+        localStorage.setItem("foreground", fgcol)
         setColor('--fg-color',fgcol);
     }
 }
@@ -21,8 +34,6 @@ function grabColors() {
 function loadColors(){
     var fg = localStorage.getItem("foreground");
     var bg = localStorage.getItem("background");
-    var se = localStorage.getItem("secondary");
-    var te = localStorage.getItem("tertiary");
     
     setColor('--fg-color',fg);
     setColor('--bg-color',bg);
