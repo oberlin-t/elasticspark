c3ed2accffb015d5573c08762998e870b5969986
oberlin-t <thomas.l.oberlin@gmail.com>
2021-02-02 22:57:07 -0500

    prevented texts from overwriting

--- js/messagecore.js
+++ js/messagecore.js
@@ -2,9 +2,16 @@ fs = require('fs');
 
 function storeMessage(username, from, message)
 {
-    fs.writeFile('js/pending/' + username + ".txt", from + " | " + message, function(err){
-        if (err) throw err;
-    });
+    if (fs.existsSync('js/pending/' + username + '.txt')){
+        fs.appendFile('js/pending/' + username + '.txt', "\n" + from + " | " + message, function(err){
+            if (err) throw err;
+        });
+    }
+    else {
+        fs.writeFile('js/pending/' + username + ".txt", from + " | " + message, function(err){
+            if (err) throw err;
+        });
+    }
 }
 
 module.exports = { storeMessage };
